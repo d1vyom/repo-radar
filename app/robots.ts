bbaa88f695e90e2407c0b87789670fc40f0c6d1b
d1vyom <divyomkumardk2007@gmail.com>
2026-08-16 21:59:17 +0530

    Changing baseUrl to my orginal one

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/api/',
     },
-    sitemap: 'https://your-domain.com/sitemap.xml',
+    sitemap: 'https://repo-radar-six.vercel.app/sitemap.xml',
   };
 }
